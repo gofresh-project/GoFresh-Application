3618e37f974a7be56809cc04358635446c890c36
Pranjali <pranjalisharma2924@gmail.com>
2026-02-01 14:42:38 +0530

    valid details

--- frontend/go-fresh-app/src/App.js
+++ frontend/go-fresh-app/src/App.js
@@ -8,14 +8,10 @@ import Home from "./pages/Home";
 import Products from "./pages/Products";
 import Vendor from "./pages/Vendor";
 import { CartProvider } from "./context/CartContext";
-<<<<<<< HEAD
 import MyOrders from "./pages/MyOrders";
 import OrderDetails from "./pages/OrderDetails";
-
-=======
 import ProductDetails from "./pages/ProductDetails";
 import CartPage from "./pages/CartPage";
->>>>>>> 6ee5750c13b7559524f103b717ee2eb966c1f0fb
 
 function App() {
   return (
@@ -29,14 +25,10 @@ function App() {
         <Route path="/register" element={<RegisterComp />} />
         <Route path="/products" element={<Products />} />
         <Route path="/vendor" element={<Vendor />} />
-<<<<<<< HEAD
          <Route path="/orders" element={<MyOrders />} />
         <Route path="/order/:id" element={<OrderDetails />} />
-        <Route path="/orders" element={<MyOrders />} />
-=======
         <Route path="/products/:productId" element={<ProductDetails />} />
         <Route path="/cart" element={<CartPage />} />
->>>>>>> 6ee5750c13b7559524f103b717ee2eb966c1f0fb
 
       </Routes>
     </CartProvider>
